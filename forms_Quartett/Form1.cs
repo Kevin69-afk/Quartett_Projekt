31b65b71f9b9edd190cb8108a01fea42b19428db
Black3is <107100761+Black3is@users.noreply.github.com>
2023-02-06 08:45:55 +0100

    Execel reader
    Tabelle

--- forms_Quartett/Form1.cs
+++ forms_Quartett/Form1.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
+using ExcelDataReader;
 
 namespace forms_Quartett
 {
@@ -16,5 +19,59 @@ namespace forms_Quartett
         {
             InitializeComponent();
         }
+        string[] name = new string[32];
+
+        int[] ps = new int[32];
+        int[] kmh = new int[32];
+        int[] speed = new int[32];
+        int[] Value = new int[32];
+        int[] weight = new int[32];
+        int[] Baujahr = new int[32];
+
+        private void setps()
+        {
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        void ExcelFileReader(string filePath)
+        {
+            FileStream stream = File.Open(filePath, FileMode.Open, FileAccess.Read);
+            IExcelDataReader excelReader;
+
+            //1. Reading Excel file
+            if (Path.GetExtension(filePath).ToUpper() == ".XLS")
+            {
+                //1.1 Reading from a binary Excel file ('97-2003 format; *.xls)
+                excelReader = ExcelReaderFactory.CreateBinaryReader(stream);
+            }
+            else
+            {
+                //1.2 Reading from a OpenXml Excel file (2007 format; *.xlsx)
+                excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+            }
+
+            //2. DataSet - The result of each spreadsheet will be created in the result.Tables
+            DataSet result;
+            result = excelReader.AsDataSet();
+
+            for (int i = 0; i < ps.Length; i++)
+            {
+                name[i] = Convert.ToString(result.Tables[0].Rows[i][0]);
+                ps[i] = Convert.ToInt32(result.Tables[0].Rows[i][1]);
+                kmh[i] = Convert.ToInt32(result.Tables[0].Rows[i][2]);
+                speed[i] = Convert.ToInt32(result.Tables[0].Rows[i][3]);
+                Value[i] = Convert.ToInt32(result.Tables[0].Rows[i][4]);
+                weight[i] = Convert.ToInt32(result.Tables[0].Rows[i][5]);
+                Baujahr[i] = Convert.ToInt32(result.Tables[0].Rows[i][6]);
+            }
+
+
+        }
+
     }
 }
+
