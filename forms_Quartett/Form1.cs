7ec2da25962b17f0b3a06a4408cf9097beb25dba
Micki09 <hamacher.mikko@gmx.de>
2023-02-08 10:46:08 +0100

    Added Player Class Added various functions to the Player

--- forms_Quartett/Form1.cs
+++ forms_Quartett/Form1.cs
@@ -53,6 +53,49 @@ namespace forms_Quartett
         {
             category = 6;
         }
+
+        void ExcelFileReader(string filePath)
+        {
+            string[] name = new string[32];
+            int[] ps = new int[32];
+            int[] kmh = new int[32];
+            int[] speed = new int[32];
+            int[] value = new int[32];
+            int[] weight = new int[32];
+            int[] baujahr = new int[32];
+
+            FileStream stream = File.Open(filePath, FileMode.Open, FileAccess.Read);
+            IExcelDataReader excelReader;
+
+            //1. Reading Excel file
+            if (Path.GetExtension(filePath).ToUpper() == ".XLS")
+            {
+                //1.1 Reading from a binary Excel file ('97-2003 format; *.xls)
+                excelReader = ExcelReaderFactory.CreateBinaryReader(stream);
+            }
+            else
+            {
+                //1.2 Reading from a OpenXml Excel file (2007 format; *.xlsx)
+                excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+            }
+
+            //2. DataSet - The result of each spreadsheet will be created in the result.Tables
+            DataSet result;
+            result = excelReader.AsDataSet();
+
+            for (int i = 0; i < ps.Length; i++)
+            {
+                name[i] = Convert.ToString(result.Tables[0].Rows[i][0]);
+                ps[i] = Convert.ToInt32(result.Tables[0].Rows[i][1]);
+                kmh[i] = Convert.ToInt32(result.Tables[0].Rows[i][2]);
+                speed[i] = Convert.ToInt32(result.Tables[0].Rows[i][3]);
+                value[i] = Convert.ToInt32(result.Tables[0].Rows[i][4]);
+                weight[i] = Convert.ToInt32(result.Tables[0].Rows[i][5]);
+                baujahr[i] = Convert.ToInt32(result.Tables[0].Rows[i][6]);
+
+                playcard[i] = new Playcard(name[i], ps[i], kmh[i], speed[i], value[i], weight[i], baujahr[i]);
+            }
+        }
     }
 }
 
